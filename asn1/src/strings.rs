b3d872d0fa815f6f114c6a49699628a3a570b904
Jonathan M. Wilbur <jonathan@wilbur.space>
2025-04-20 14:27:37 -0400

    feat(asn1): add is_ia5_str, is_general_str, and is_visible_str

--- asn1/src/strings.rs
+++ asn1/src/strings.rs
@@ -1,4 +1,5 @@
 
+#[inline]
 pub fn is_printable_char (b: u8) -> bool {
     b.is_ascii_alphanumeric()
     || (b >= b'\x27' && b < b'0' && b != b'*') // '()+,-./ BUT NOT *
@@ -8,16 +9,32 @@ pub fn is_printable_char (b: u8) -> bool {
     || b == b'?'
 }
 
+#[inline]
 pub fn is_printable_str (s: &str) -> bool {
     s.as_bytes().iter().all(|b| is_printable_char(*b))
 }
 
+#[inline]
 pub fn is_numeric_char (b: u8) -> bool {
     b.is_ascii_digit() || b == b' '
 }
 
+#[inline]
 pub fn is_numeric_str (s: &str) -> bool {
     s.as_bytes().iter().all(|b| is_numeric_char(*b))
 }
 
-// TODO: Do more for other string types.
+#[inline]
+pub fn is_ia5_str (s: &str) -> bool {
+    s.is_ascii()
+}
+
+#[inline]
+pub fn is_general_str (s: &str) -> bool {
+    s.is_ascii()
+}
+
+#[inline]
+pub fn is_visible_str (s: &str) -> bool {
+    s.chars().all(|c| c.is_ascii_graphic())
+}
